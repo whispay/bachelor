39005e1081387b8462bf76e9fd3657ac2b618822
Dschialin <Dschialin.phan@hotmail.com>
2020-10-27 03:04:56 +0100

    test

--- sap-web/src/views/Home.vue
+++ sap-web/src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-
+    {{Schemas}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
+
 
 export default {
 
-}
+  components: {},
+  
+  data() {
+    return {
+      Schemas: null,
+    };
+  },
+  methods: {
+    
+  },
+  mounted() {
+    // fetch data from a url endpoint
+    const response = axios.get("http://localhost:3000/");
+
+    this.Schemas = response;
+  },
+};
 </script>
