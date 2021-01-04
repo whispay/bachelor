9f26ad6c5abea96bac1a668ba318d55e1ac293e6
Dschialin <Dschialin.phan@hotmail.com>
2021-01-04 17:20:08 +0100

    map

---
+++ sap-web/src/views/Map.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="map">
+    <h1>Map</h1>
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9717.541066803851!2d13.528142959807209!3d52.49026528326414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8492e8e935b15%3A0xa0d5d0da2ff283a6!2sHochschule%20f%C3%BCr%20Technik%20und%20Wirtschaft%20Berlin%20(HTW%20Berlin)%20-%20Campus%20Treskowallee!5e0!3m2!1sde!2sde!4v1609772443046!5m2!1sde!2sde" width="800" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </div>
+</template>
